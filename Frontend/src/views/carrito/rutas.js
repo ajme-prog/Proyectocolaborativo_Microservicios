1039e0c3c11c050f85ab687b3d35639890e05b8d
Kevin <kevinalexandergarcia1999@gmail.com>
2021-06-19 12:50:40 -0600

    Cambio ip

--- Frontend/src/views/carrito/rutas.js
+++ Frontend/src/views/carrito/rutas.js
@@ -1,7 +1,7 @@
 let host ="localhost"
 
 export let URL = {
-    obtener_productos: `http://${host}:9000/carrito/obtener_libros`,
-    generar_pedido: `http://${host}:9000/carrito/generar_pedido`,
-    obtener_compras: `http://${host}:9000/carrito/obtener_compras/`
+    obtener_productos: `http://localhost:9000/carrito/obtener_libros`,
+    generar_pedido: `http://localhost:9000/carrito/generar_pedido`,
+    obtener_compras: `http://localhost:9000/carrito/obtener_compras/`
 };
