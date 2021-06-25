a836c6e4ce205ad3a0569b1a630482aca658f8c3
Ronald Romero <3606169920101@ingenieria.usac.edu.gt>
2021-06-25 14:01:18 -0600

    Se estructuró el esb

---
+++ ServicioESB/index.js
@@ -0,0 +1,25 @@
+const cors = require("cors");
+const express = require("express");
+const fetch = require('node-fetch');
+const app = express();
+app.use(express.json());
+app.use(cors());
+const url_autenticacion = "http://localhost:3001"
+
+app.post('/login', async(req, res) => {
+    console.log(req.body);
+    const query = await fetch(`${url_autenticacion}/login`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            'Access-Control-Allow-Origin': '*',
+        },
+        body:JSON.stringify(req.body)
+    });
+    const result= await query.json();
+    console.log(result); 
+    res.status(query.status).json(result);
+});
+
+app.listen(process.env.PORT || 4091);
